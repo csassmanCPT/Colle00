ff9b35ecbe719d82672ccc37eed89c611a79b98f
Cameron Sassman <csassman@c3r8s5.wethinkcode.co.za>
2020-07-14 11:40:41 +0200

    rush00.c

---
+++ rush00.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csassman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/14 11:38:02 by csassman          #+#    #+#             */
+/*   Updated: 2020/07/14 11:40:20 by csassman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+void     rush(int x, int y) {
+    int     xindx = 1;
+    int     yindx = 2;
+    while (xindx <= x) {
+        if (xindx == 1 || xindx == x) {
+            ft_putchar('o');
+        } else {
+            ft_putchar('-');
+            }
+        xindx++;
+        }
+    xindx = 2;
+    ft_putchar('\n');
+    while (yindx < y)
+    {
+        ft_putchar('|');
+        while (xindx < x)
+        {
+            ft_putchar(' ');
+            xindx++;
+        }
+        xindx = 2;
+        ft_putchar('|');
+        ft_putchar('\n');
+        yindx++;
+    }
+    xindx = 1;
+    while (xindx <= x)
+    {
+        if (y == 1) {
+            xindx = x;
+        }
+        else if (xindx == 1 || xindx == x) {
+            ft_putchar('o');
+        } else {
+            ft_putchar('-');
+        }
+        xindx++;
+    }
+    ft_putchar('\n');
+}
